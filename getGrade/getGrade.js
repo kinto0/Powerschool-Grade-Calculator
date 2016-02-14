ca5fa6b9fec6e29c586a804ad1def86c09090954
kyleai <kyleatyahoo@gmail.com>
2016-02-13 20:46:12 -0500

    nice man on stack overflow fixed it

--- getGrade/getGrade.js
+++ getGrade/getGrade.js
@@ -1,24 +1,30 @@
 
-$(document).ready(function() {
-	var arr = [];
-	var numerator = 0;
-	var denominator = 0;
-	i = 0;
 
-	//add value of all elements in assignmentScores that are centered to arr (grades are the only centered thing)
-	$('#assignmentScores').each(function()
-        if(isNaN($(this).attr('td [align="center"]')){
-            arr[i++] = $(this).attr('td [align="center"]');
-        }
-  	);
-
-	//for every item in the array, add up numerators and denominators
-	for each(var item in numerator){
-		numerator += arr[item].substring(0, arr[item].indexOf("/")-1);
-		denominator += arr[item].substring(arr[item].indexOf("/")+1, arr[item].length);
-	}
-
-	$('#assignmentScores h1').append(numerator/denominator);
-
+// We're going to do a lot of work, so let's get table in a variable
+var table = $('#assignmentScores table');
+// Which column is the score in? Find it here
+var score_column = table.find('th:contains("Score")').index();
+// Set up a regular expression for testing that the column contains something like '30/30'
+var regex = /(\d+)\/(\d+)/;
+// initialize the numerator and denomenator
+var num = 0, den = 0;
+// loop over all the table rows
+table.find('tr').each(
+function() {
+  // get the value from the column that is the "Score" column
+  var score = $(this).find('td:eq(' + score_column + ')').html();
+  // Run the regular expression to get the matches
+  var matches = regex.exec(score);
+  // If there are matches, add them to numerator and denomenator
+  if ($.isArray(matches) && typeof matches[2] != 'undefined') {
+    num+= +matches[1];
+    den+= +matches[2];
+  }
 });
 
+// Calculate the percent - but only if we actually have a numerator / denomenator
+percent = (num && den) ? (num / den) * 100 : 0;
+// And, for fun, append the values to the table
+table.append('<tr><td colspan="' + score_column + '" style="text-align:right;">Total:</td><td>' + percent.toFixed(1) + '%</td></tr>');
+
+//Thank you to cale_b on stackoverflow for helping (doing the whole thing)
